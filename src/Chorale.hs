bbda7db9ea9c9f9b3f332bf914c83aa762f68903
Oscar South <oscarsouth@gmail.com>
2018-08-04 12:49:14 +0100

    renaming

--- src/Chorale.hs
+++ src/Chorale.hs
@@ -10,7 +10,7 @@ readNoteName  :: String -> NoteName
 readNoteName s = read $ replace "#" "'" s
 
 -- |mapping from sets of fundamentals and overtones into lists of viable triads
--- bachTriads        :: (Eq a, Num a) => a -> [String] -> [[Integer]] -> [[[Integer]]]
+bachTriads        :: (Eq a, Num a) => a -> [String] -> [[Integer]] -> [[[Integer]]]
 bachTriads n rs ps  =
   let fund = (\x -> [x]) . i . readNoteName <$> rs
    in zipWith (overtoneSets n) fund ps
@@ -19,10 +19,10 @@ bachTriads n rs ps  =
 dissonanceLevel                  :: [Integer] -> Integer
 dissonanceLevel xs
   | countElem iVect 0 == 5 = 27
-  | otherwise           = sum $ zipWith (*) dissonanceVect vect
+  | otherwise           = sum $ zipWith (*) dissVect iVect
     where
       iVect        = intervalVector xs 
-      dissVect    = [16,8,4,2,1,24] -- based on work of Paul Hindemith
+      dissVect     = [16,8,4,2,1,24] -- based on work of Paul Hindemith
 
 
 
